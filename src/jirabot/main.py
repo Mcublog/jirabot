c62a067d0cb928ca4cf23261595150d2a289ec2f
Viacheslav <v.konovalov@crystals.ru>
2024-10-08 14:46:55 +0300

    added worktime counting;

--- src/jirabot/main.py
+++ src/jirabot/main.py
@@ -1,23 +1,58 @@
 #!/usr/bin/env python
 
 import os
+from datetime import datetime, timedelta, timezone
 
-from jira import JIRA
+from jira import JIRA, Worklog, exceptions
 
 USER_EMAIL = os.environ.get('JIRA_EMAIL')
 USER_TOKEN = os.environ.get('JIRA_TOKEN')
+JIRA_SITE = os.environ.get('JIRA_SITE')
+
+
+def summary_print(timetrack: int):
+    hours, remainder = divmod(timetrack, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    print(
+        f"Summary: {int(hours):02d}h {int(minutes):02d}m {int(seconds):02d}s")
+
+
+def get_worklogs_by_user_and_week(jira: JIRA) -> list[Worklog]:
+    worklogs = []
+    for issue in jira.search_issues(
+            'WorkLogAuthor = currentUser() and WorkLogDate > startOfWeek(-1) order by created desc',
+    ):
+        print('{}: {}'.format(issue.key, issue.fields.summary))
+        worklogs += issue.fields.worklog.worklogs
+
+    if not worklogs:
+        print("worklogs is empty")
+        return []
+
+    worklogs_by_week: list[Worklog] = []
+    for w in worklogs:
+        t = datetime.fromisoformat(w.created)
+        if t < datetime.now(timezone.utc) - timedelta(days=6):
+            continue
+        worklogs_by_week.append(w)
+
+    return worklogs_by_week
 
 
 def main():
-    jira = JIRA('https://crystals.atlassian.net', basic_auth=(USER_EMAIL, USER_TOKEN))
-    issue = jira.issue('KKT-1090')
-    print(issue.fields.description)
-    for w in issue.fields.worklog.worklogs:
-        print("-" * 15)
-        print(w.created)
-        print(w.timeSpent)
-        print(w.comment)
-        print("-" * 15)
+    try:
+        jira = JIRA(JIRA_SITE, basic_auth=(USER_EMAIL, USER_TOKEN))
+        jira.myself()
+    except exceptions.JIRAError as e:
+        print(e.text)
+        return -1
+
+    worklogs = get_worklogs_by_user_and_week(jira=jira)
+
+    timetrack = sum([w.timeSpentSeconds for w in worklogs])
+
+    summary_print(timetrack)
+
 
 if __name__ == "__main__":
     main()
