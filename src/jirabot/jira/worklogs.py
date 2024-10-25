f043cbf98ee5ecab72280d28ea7c4ca5cd25f8d2
Viacheslav <slava.k@ks2corp.com>
2024-10-25 21:42:20 +0300

    fixed: worklogs overflow;

--- src/jirabot/jira/worklogs.py
+++ src/jirabot/jira/worklogs.py
@@ -3,23 +3,14 @@ from datetime import datetime, timedelta, timezone
 from jira import JIRA, Issue, Worklog
 
 
-def get_issues_by_user_and_week(jira: JIRA) -> list[Issue]:
-    issues = []
-    DB_REQUEST = '''WorkLogAuthor = currentUser() and
-    WorkLogDate > startOfWeek(-1) order by created desc'''
-    for issue in jira.search_issues(DB_REQUEST, ):
-        issues.append(issue)
-    return issues
-
-
-def get_by_user_and_week(issues: list[Issue]) -> list[Worklog]:
+def get_worklogs_by_issues(jira: JIRA, issues: list[Issue]) -> list[Worklog]:
     worklogs = []
-    for issue in issues:
-        worklogs += issue.fields.worklog.worklogs
+    for i in issues:
+        worklogs += jira.worklogs(issue=i.key)
+    return worklogs
 
-    if not worklogs:
-        return []
 
+def by_week(worklogs: list[Worklog]) -> list[Worklog]:
     worklogs_by_week: list[Worklog] = []
     now = datetime.now(timezone.utc).replace(hour=0,
                                              minute=0,
@@ -33,5 +24,13 @@ def get_by_user_and_week(issues: list[Issue]) -> list[Worklog]:
         if t < now - timedelta(days=7):
             continue
         worklogs_by_week.append(w)
-
     return worklogs_by_week
+
+
+def get_issues_by_user_and_week(jira: JIRA) -> list[Issue]:
+    issues = []
+    DB_REQUEST = '''WorkLogAuthor = currentUser() and
+    WorkLogDate > startOfWeek(-1) order by created desc'''
+    for issue in jira.search_issues(DB_REQUEST, fields=None):
+        issues.append(issue)
+    return issues
